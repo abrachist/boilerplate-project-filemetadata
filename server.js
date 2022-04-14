754a5f3fcebb67444751d9e9a90748fb9de0ce8a
abdurrachman <manharist@gmail.com>
2022-04-14 07:20:19 +0000

    File metadata microservice challenge

--- server.js
+++ server.js
@@ -1,18 +1,31 @@
 var express = require('express');
 var cors = require('cors');
+var multer  = require('multer')
 require('dotenv').config()
 
 var app = express();
 
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
+const upload = multer({ dest: './uploads/' })
 
 app.get('/', function (req, res) {
     res.sendFile(process.cwd() + '/views/index.html');
 });
 
-
-
+// api route to upload a file
+app.post('/api/fileanalyse', upload.single('upfile'), function (req, res) {
+  if(req.file){
+    res.json({
+      "name": req.file.originalname,
+      "type": req.file.mimetype,
+      "size": req.file.size
+    });
+  } else {
+    res.json({ "error": "No file uploaded" });
+  }
+  
+});
 
 const port = process.env.PORT || 3000;
 app.listen(port, function () {
